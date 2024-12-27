40d8b093a00a980c4387fac48984768d75697f62
zkl10 <govada.elton@gmail.com>
2024-12-26 21:13:59 -0500

    Fix update points on initial controller connect and clean up code

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,5 @@
 <script lang="ts">
-  import {
-    Account,
-    RpcProvider,
-    Contract,
-    json,
-    type BigNumberish,
-    type Uint256,
-    cairo,
-    AccountInterface,
-  } from "starknet";
+  import { RpcProvider, Contract, AccountInterface } from "starknet";
   import { onMount } from "svelte";
   import Controller, { type SessionPolicies } from "@cartridge/controller";
 
@@ -16,17 +7,6 @@
   const providerKatanaDev = new RpcProvider({
     nodeUrl: rpcUrl,
   });
-
-  const userPK1 =
-    "0x3e3979c1ed728490308054fe357a9f49cf67f80f9721f44cc57235129e090f4";
-  const userAddr1 =
-    "0x6677fe62ee39c7b07401f754138502bab7fac99d2d3c5d37df7d1c6fab10819";
-  const acc1 = new Account(providerKatanaDev, userAddr1, userPK1);
-  // const userPK2 =
-  //   "0x1c9053c053edf324aec366a34c6901b1095b07af69495bffec7d7fe21effb1b";
-  // const userAddr2 =
-  //   "0x13d9ee239f33fea4f8785b9e3870ade909e20a9599ae7cd62c1c292b73af1b7";
-  // const acc2 = new Account(providerKatanaDev, userAddr2, userPK2);
 
   const mainContractAddr =
     "0x5fe3cd9b8a92c16fee81e08579cc87c1328aa9a375b7d1f472ee183af4e12b8";
@@ -71,29 +51,11 @@
   );
 
   onMount(async () => {
-    // version 1
-    // mainContractClass = await providerKatanaDev.getClassAt(mainContractAddr);
-    // mainContract = new Contract(mainContractClass.abi, mainContractAddr, acc1);
-    // mainContract.connect(acc1);
-    // const connectedAddress = acc1.address;
-    // console.log(`Account ${connectedAddress}: Connected to main contract`);
-    // updatedPoints = await mainContract.get_points(connectedAddress);
-
-    // version 2
     mainContractClass = await providerKatanaDev.getClassAt(mainContractAddr);
 
     if (await controller.probe()) {
       // auto connect
       await connectController();
-      mainContract = new Contract(
-        mainContractClass.abi,
-        mainContractAddr,
-        executeAccount,
-      );
-      if (executeAccount) {
-        mainContract.connect(executeAccount);
-        updatedPoints = await mainContract.get_points(executeAccount.address);
-      }
     }
     loading = false;
   });
@@ -120,6 +82,15 @@
         connectedController = controller;
         executeAccount = controller.account;
         controllerUsername = await controller.username();
+        mainContract = new Contract(
+          mainContractClass.abi,
+          mainContractAddr,
+          executeAccount,
+        );
+        if (executeAccount) {
+          mainContract.connect(executeAccount);
+          updatedPoints = await mainContract.get_points(executeAccount.address);
+        }
       }
     } catch (e) {
       console.log(e);
@@ -142,9 +113,7 @@
           calldata: [],
         },
       ]);
-      if (result) {
-        transactionList.push(result.transaction_hash);
-      }
+      transactionList.push(result.transaction_hash);
     } catch (e) {
       console.log(e);
     }
@@ -160,9 +129,7 @@
           calldata: [guess],
         },
       ]);
-      if (result) {
-        transactionList.push(result.transaction_hash);
-      }
+      transactionList.push(result.transaction_hash);
     } catch (e) {
       console.log(e);
     }
@@ -178,40 +145,11 @@
           calldata: [],
         },
       ]);
-      if (result) {
-        transactionList.push(result.transaction_hash);
-      }
+      transactionList.push(result.transaction_hash);
     } catch (e) {
       console.log(e);
     }
   }
-
-  // old functions
-  // async function startGame() {
-  //   const t1 = await mainContract.start_game();
-  //   await providerKatanaDev.waitForTransaction(t1.transaction_hash);
-  //   const updated_points: bigint = await mainContract.get_points(acc1.address);
-  //   updatedPoints = updated_points;
-  // }
-
-  // async function guessShort() {
-  //   const t1 = await mainContract.gamble(false);
-  //   const updated_points: BigInt = await mainContract.get_points(acc1.address);
-  //   updatedPoints = updated_points;
-  // }
-
-  // async function guessLong() {
-  //   const t1 = await mainContract.gamble(true);
-  //   const updated_points: BigInt = await mainContract.get_points(acc1.address);
-  //   updatedPoints = updated_points;
-  // }
-
-  // async function resetGame() {
-  //   const t1 = await mainContract.reset_game();
-  //   await providerKatanaDev.waitForTransaction(t1.transaction_hash);
-  //   const updated_points: bigint = await mainContract.get_points(acc1.address);
-  //   updatedPoints = updated_points;
-  // }
 </script>
 
 <main>
@@ -227,12 +165,15 @@
       {/if}
     </div>
     {#if connectedController}
-      <h1>{pointsAsUSD} $FAKE</h1>
+      <p></p>
       <div>
         <button onclick={executeStartGame}>Start Game</button>
+        <button onclick={executeResetGame}>Reset Game</button>
+      </div>
+      <h1>{pointsAsUSD} $FAKE</h1>
+      <div>
         <button onclick={() => executeGamble(false)}>Guess Short</button>
         <button onclick={() => executeGamble(true)}>Guess Long</button>
-        <button onclick={executeResetGame}>Reset Game</button>
       </div>
     {/if}
   </div>
